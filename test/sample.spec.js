b4e8df4f24c72e26c19856fbacd42093c7a99aa8
Bernardo Guerreiro <bernardoguerr@gmail.com>
2020-08-31 22:59:52 +0100

    implement auth

--- test/sample.spec.js
+++ test/sample.spec.js
@@ -5,24 +5,26 @@ const chaiResponseValidator = require('chai-openapi-response-validator');
 const expect = chai.expect;
 chai.use(chaiResponseValidator(path.join(__dirname, '../openapi.yaml')));
 
-
 const { baseURL } = require('../config');
+const { getAccessToken } = require('../lib/authorization');
 
 describe('JSON-Placeholder API Tests', () => {
     let api;
+    let bearerToken;
 
     before(async () => {
         api = supertest(baseURL)
+        // bearerToken = await getAccessToken();
+        bearerToken = 'test';
     })
 
     describe('POST /posts endpoint', () => {
         it('should return 201 when post with no body', async () => {
             const response = await api
                 .post('/posts')
-                // .set('Authorization', `Bearer ${}`)
-                // .expect(201)
+                .set('Authorization', `Bearer ${bearerToken}`)
 
-            expect(response.statusCode).toBe(201);
+            expect(response.statusCode).to.equal(201);
             expect(response).to.satisfyApiSpec
         })
     })
